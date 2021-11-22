802ff7925ebe7e9ca58d1b8ca50b62d8d079445c
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2021-11-22 20:06:27 +0200

    complete 2.22 DTOs

---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,4 @@
+export class CreateTaskDto {
+  title: string;
+  desciption: string;
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,13 +11,17 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, desciption: string): Task {
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, desciption } = createTaskDto;
+
     const task: Task = {
-      id: '',
+      id: uuid(),
       title,
       desciption,
       status: TaskStatus.OPEN,
     };
+
+    this.tasks.push(task);
 
     return task;
   }
