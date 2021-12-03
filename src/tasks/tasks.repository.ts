ef4c73815a1e5a1194b63501b91419800b40b102
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2021-12-03 19:30:39 +0200

    complete new get all tasks functionality

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -8,10 +8,11 @@ import { Task } from './task.entity';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
 
     const query = this.createQueryBuilder('task');
+    query.where({ user });
 
     if (status) {
       // By writting ":status" we are defining a variable for our query
@@ -22,7 +23,7 @@ export class TasksRepository extends Repository<Task> {
 
     if (search) {
       query.andWhere(
-        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
         { search: `%${search}%` },
       );
     }
